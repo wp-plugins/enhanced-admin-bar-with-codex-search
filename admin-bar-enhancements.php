841976bbf993208e40773141081f4466e32813f8
jtsternberg <jtsternberg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-13 14:49:04 +0000

    version 1.5
    git-svn-id: https://plugins.svn.wordpress.org/enhanced-admin-bar-with-codex-search/trunk@396697 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin-bar-enhancements.php
+++ admin-bar-enhancements.php
@@ -2,11 +2,11 @@
 /*
 Plugin Name: Enhanced Admin Bar with Codex Search
 Plugin URI: http://dsgnwrks.pro/enhanced-admin-bar-with-codex-search/
-Description: This plugin provided easy access to the codex, plugins repository and common wp-admin areas via the 3.1 Admin Bar.
+Description: This plugin adds convenient search fields to provide easy access to the codex, wpbeginner, and common wp-admin areas via the 3.1 Admin Bar.
 Author URI: http://dsgnwrks.pro
 Author: DsgnWrks
 Donate link: http://dsgnwrks.pro/give/
-Version: 1.0
+Version: 1.5
 */
 
 
@@ -21,9 +21,12 @@ function dsgnwrks_adminbar_search_css() {
 add_filter( 'in_admin_header', 'dsgnwrks_3_2_search_formS', 11 );
 function dsgnwrks_3_2_search_formS() {
 	if ( get_bloginfo('version') >= 3.2 ) {
+		// install_search_form2();
 	    echo '
-		<form target="_blank" method="get" action="http://wordpress.org/extend/plugins/search.php" class="alignright dw_search_form search_plugins" >
-			<input type="text" onblur="this.value=(this.value==\'\') ? \'Search Plugins\' : this.value;" onfocus="this.value=(this.value==\'Search Plugins\') ? \'\' : this.value;" value="Search Plugins" name="search" class="text dw_search_input" > 
+		<form method="get" action="'.admin_url('plugin-install.php').'"  class="alignright dw_search_form search_plugins" >
+			<input type="hidden" name="tab" value="search"/>
+			<input type="hidden" name="type" value="term"/>
+			<input type="text" placeholder="Search Plugins" onblur="this.value=(this.value==\'\') ? \'Search Plugins\' : this.value;" onfocus="this.value=(this.value==\'Search Plugins\') ? \'\' : this.value;" value="Search Plugins" name="s" value="' . esc_attr($term) . '" class="text dw_search_input" />
 			<input type="submit" value="Go" class="button dw_search_go"  />
 		</form>
 		<form target="_blank" method="get" action="http://wordpress.org/search/do-search.php" class="alignright dw_search_form search_codex" >
@@ -34,16 +37,51 @@ function dsgnwrks_3_2_search_formS() {
 	}
 }
 
+// Add Custom Menu Option
+add_action('init', 'dsgnwrks_adminbar_nav');
+function dsgnwrks_adminbar_nav() {
+
+	register_nav_menus( array(
+		'admin_bar_nav' => __( 'Admin Bar Custom Navigation Menu' ),
+	) );
+
+}
+
 // Add Custom Menu to the Admin bar
 add_action('admin_bar_init', 'dsgnwrks_adminbar_menu_init');
 function dsgnwrks_adminbar_menu_init() {
 	if (!is_super_admin() || !is_admin_bar_showing() )
 		return;
- 	add_action( 'admin_bar_menu', 'dsgnwrks_admin_bar_menu', 500 );
+ 	add_action( 'admin_bar_menu', 'dsgnwrks_admin_bar_menu', 1000 );
 }
 
 function dsgnwrks_admin_bar_menu() {
 	global $wp_admin_bar;
+
+	// Add a custom menu option
+	$menu_name = 'admin_bar_nav';
+	if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) {
+		$menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
+
+	    $menu_items = wp_get_nav_menu_items( $menu->term_id );
+	    $wp_admin_bar->add_menu( array(
+	        'id' => 'dsgnwrks-admin-menu-0',
+	        'title' => 'Enhanced Admin Bar Custom Menu',
+			'href' => '#' ) );
+	    foreach ( $menu_items as $menu_item ) {
+	        $wp_admin_bar->add_menu( array(
+	            'id' => 'dsgnwrks-admin-menu-' . $menu_item->ID,
+	            'parent' => 'dsgnwrks-admin-menu-' . $menu_item->menu_item_parent,
+	            'title' => $menu_item->title,
+	            'href' => $menu_item->url,
+	            'meta' => array(
+	                'title' => $menu_item->attr_title,
+	                'target' => $menu_item->target,
+	                'class' => implode( ' ', $menu_item->classes ),
+	            ),
+	        ) );
+	    }
+	}
 
 	$go_button = '<input type="submit" value="Go" class="button dw_search_go"  /></form>';
 	$admin_url = get_admin_url();
@@ -61,48 +99,217 @@ function dsgnwrks_admin_bar_menu() {
 	$wp_admin_bar->add_menu( array(
 	'parent' => 'dsgnwrks_help_menu',
 	'title' => __( '
-	<strong style="display:none;">Search Plugins</strong>
-	<form target="_blank" method="get" action="http://wordpress.org/extend/plugins/search.php" class="alignleft dw_search">
-		<input type="text" onblur="this.value=(this.value==\'\') ? \'Search Plugins\' : this.value;" onfocus="this.value=(this.value==\'Search Plugins\') ? \'\' : this.value;" value="Search Plugins" name="search" class="text dw_search_input" > 
+	<strong style="display:none;">Search wpbeginner.com</strong>
+	<form target="_blank" method="get" action="http://www.wpbeginner.com/" class="alignleft dw_search" >
+		<input type="text" onblur="this.value=(this.value==\'\') ? \'Search wpbeginner.com\' : this.value;" onfocus="this.value=(this.value==\'Search wpbeginner.com\') ? \'\' : this.value;" value="Search wpbeginner.com" name="s" class="text dw_search_input" > 
 	'.$go_button),
 	'href' => '#' ) );
+
+	if ( !is_admin() ) {
+
+		$wp_admin_bar->add_menu( array(
+		'id' => 'plugins_stuff',
+		'parent' => 'dsgnwrks_help_menu',
+		'title' => __( 'Plugins'),
+		'href' => admin_url('plugins.php') ) );
+
+		$wp_admin_bar->add_menu( array(
+		'parent' => 'plugins_stuff',
+		'title' => __( '
+		<strong style="display:none;">Search Plugins</strong>
+		<form method="get" action="'.admin_url('plugin-install.php?tab=search').'"  class="alignleft dw_search" >
+		<input type="hidden" name="tab" value="search"/>
+		<input type="hidden" name="type" value="term"/>
+		<input type="text" placeholder="Search Plugins" onblur="this.value=(this.value==\'\') ? \'Search Plugins\' : this.value;" onfocus="this.value=(this.value==\'Search Plugins\') ? \'\' : this.value;" value="Search Plugins" name="s" value="' . esc_attr($term) . '" class="text dw_search_input" />
+		<label class="screen-reader-text" for="plugin-search-input">' . _e('Search Plugins') . '</label>'.$go_button),
+		'href' => '#' ) );
+
+		$wp_admin_bar->add_menu( array(
+		'parent' => 'plugins_stuff',
+		'title' => __( 'Upload Plugin' ),
+		'href' => admin_url('plugin-install.php?tab=upload') ) );
+
+		$wp_admin_bar->add_menu( array(
+		'id' => 'themes_stuff',
+		'parent' => 'dsgnwrks_help_menu',
+		'title' => __( 'Themes'),
+		'href' => admin_url('themes.php') ) );
+
+		$wp_admin_bar->add_menu( array(
+		'parent' => 'themes_stuff',
+		'title' => __( '
+		<strong style="display:none;">Search Themes</strong>
+		<form method="get" action="'.admin_url('theme-install.php?tab=search').'"  class="alignleft dw_search" >
+		<input type="hidden" name="tab" value="search"/>
+		<input type="hidden" name="type" value="term"/>
+		<input type="text" placeholder="Search Themes" onblur="this.value=(this.value==\'\') ? \'Search Themes\' : this.value;" onfocus="this.value=(this.value==\'Search Themes\') ? \'\' : this.value;" value="Search Themes" name="s" value="' . esc_attr($term) . '" class="text dw_search_input" />
+		<label class="screen-reader-text" for="theme-search-input">' . _e('Search Themes') . '</label>'.$go_button),
+		'href' => '#' ) );
+
+		$wp_admin_bar->add_menu( array(
+		'parent' => 'themes_stuff',
+		'title' => __( 'Upload Theme' ),
+		'href' => admin_url('theme-install.php?tab=upload') ) );
+
+		$wp_admin_bar->add_menu( array(
+		'id' => 'media_stuff',
+		'parent' => 'dsgnwrks_help_menu',
+		'title' => __( 'Media'),
+		'href' => admin_url('upload.php') ) );
+
+		$wp_admin_bar->add_menu( array(
+		'parent' => 'media_stuff',
+		'title' => __( '
+		<strong style="display:none;">Search Media</strong>
+		<form method="get" action="'.admin_url('upload.php?tab=search').'"  class="alignleft dw_search" >
+		<input type="text" placeholder="Search Media" onblur="this.value=(this.value==\'\') ? \'Search Media\' : this.value;" onfocus="this.value=(this.value==\'Search Media\') ? \'\' : this.value;" value="Search Media" name="s" value="' . esc_attr($term) . '" class="text dw_search_input" />
+		<label class="screen-reader-text" for="media-search-input">' . _e('Search Media') . '</label>'.$go_button),
+		'href' => '#' ) );
+
+		$wp_admin_bar->add_menu( array(
+		'parent' => 'media_stuff',
+		'title' => __( 'Upload Media' ),
+		'href' => admin_url('media-new.php') ) );
+
+	} else {
+
+		$wp_admin_bar->add_menu( array(
+		'id' => 'plugins_stuff',
+		'parent' => 'dsgnwrks_help_menu',
+		'title' => __( '
+		<strong style="display:none;">Search Plugins</strong>
+		<form method="get" action="'.admin_url('plugin-install.php?tab=search').'"  class="alignleft dw_search" >
+		<input type="hidden" name="tab" value="search"/>
+		<input type="hidden" name="type" value="term"/>
+		<input type="text" placeholder="Search Plugins" onblur="this.value=(this.value==\'\') ? \'Search Plugins\' : this.value;" onfocus="this.value=(this.value==\'Search Plugins\') ? \'\' : this.value;" value="Search Plugins" name="s" value="' . esc_attr($term) . '" class="text dw_search_input" />
+		<label class="screen-reader-text" for="plugin-search-input">' . _e('Search Plugins') . '</label>'.$go_button),
+		'href' => '#' ) );
+
+		$wp_admin_bar->add_menu( array(
+		'parent' => 'plugins_stuff',
+		'title' => __( 'Upload Plugin' ),
+		'href' => admin_url('plugin-install.php?tab=upload') ) );
+
+		$wp_admin_bar->add_menu( array(
+		'id' => 'themes_stuff',
+		'parent' => 'dsgnwrks_help_menu',
+		'title' => __( '
+		<strong style="display:none;">Search Themes</strong>
+		<form method="get" action="'.admin_url('theme-install.php?tab=search').'"  class="alignleft dw_search" >
+		<input type="hidden" name="tab" value="search"/>
+		<input type="hidden" name="type" value="term"/>
+		<input type="text" placeholder="Search Themes" onblur="this.value=(this.value==\'\') ? \'Search Themes\' : this.value;" onfocus="this.value=(this.value==\'Search Themes\') ? \'\' : this.value;" value="Search Themes" name="s" value="' . esc_attr($term) . '" class="text dw_search_input" />
+		<label class="screen-reader-text" for="theme-search-input">' . _e('Search Themes') . '</label>'.$go_button),
+		'href' => '#' ) );
+
+		$wp_admin_bar->add_menu( array(
+		'parent' => 'themes_stuff',
+		'title' => __( 'Upload Theme' ),
+		'href' => admin_url('theme-install.php?tab=upload') ) );
+
+		$wp_admin_bar->add_menu( array(
+		'id' => 'media_stuff',
+		'parent' => 'dsgnwrks_help_menu',
+		'title' => __( '
+		<strong style="display:none;">Search Media</strong>
+		<form method="get" action="'.admin_url('upload.php?tab=search').'"  class="alignleft dw_search" >
+		<input type="text" placeholder="Search Media" onblur="this.value=(this.value==\'\') ? \'Search Media\' : this.value;" onfocus="this.value=(this.value==\'Search Media\') ? \'\' : this.value;" value="Search Media" name="s" value="' . esc_attr($term) . '" class="text dw_search_input" />
+		<label class="screen-reader-text" for="media-search-input">' . _e('Search Media') . '</label>'.$go_button),
+		'href' => '#' ) );
+
+		$wp_admin_bar->add_menu( array(
+		'parent' => 'media_stuff',
+		'title' => __( 'Upload Media' ),
+		'href' => admin_url('media-new.php') ) );		
+	}
+
+	$actions = array();
+	foreach ( (array) get_post_types( array( 'show_ui' => true ), 'objects' ) as $ptype_obj ) {
+		if ( true !== $ptype_obj->show_in_menu || ! current_user_can( $ptype_obj->cap->edit_posts ) )
+			continue;
+
+		$actions[ 'post-new.php?post_type=' . $ptype_obj->name ] = array( $ptype_obj->labels->name, $ptype_obj->cap->edit_posts, 'new-' . $ptype_obj->name, $ptype_obj->labels->singular_name, $ptype_obj->name, 'edit.php?post_type=' . $ptype_obj->name );
+	}
+
+	if ( empty( $actions ) )
+		return;
+
+	foreach ( $actions as $link => $action ) {
+
+		if ( is_admin() ) {
+			$wp_admin_bar->add_menu( array( 'parent' => 'dsgnwrks_help_menu', 
+			'title' => __( '
+			<strong style="display:none;">Search '.$action[0].'</strong>
+			<form method="get" action="'.admin_url('edit.php').'"  class="alignleft dw_search" >
+			<input type="hidden" name="post_status" value="all"/>
+			<input type="hidden" name="post_type" value="'.$action[4].'"/>
+			<input type="text" placeholder="Search '.$action[0].'" onblur="this.value=(this.value==\'\') ? \'Search '.$action[0].'\' : this.value;" onfocus="this.value=(this.value==\'Search '.$action[0].'\') ? \'\' : this.value;" value="Search '.$action[0].'" name="s" value="' . esc_attr($term) . '" class="text dw_search_input" />
+			<label class="screen-reader-text" >' . _e('Search '.$action[0]) . '</label>'.$go_button),
+			'href' => '#' ) );
+		} else {
+			$wp_admin_bar->add_menu( array( 
+			'parent' => 'dsgnwrks_help_menu', 
+			'id' => $action[2], 
+			'title' => $action[0], 
+			'href' => admin_url($action[5]) ) );
+
+
+			$wp_admin_bar->add_menu( array( 'parent' => $action[2], 
+			'title' => __( '
+			<strong style="display:none;">Search '.$action[0].'</strong>
+			<form method="get" action="'.admin_url('edit.php').'"  class="alignleft dw_search" >
+			<input type="hidden" name="post_status" value="all"/>
+			<input type="hidden" name="post_type" value="'.$action[4].'"/>
+			<input type="text" placeholder="Search '.$action[0].'" onblur="this.value=(this.value==\'\') ? \'Search '.$action[0].'\' : this.value;" onfocus="this.value=(this.value==\'Search '.$action[0].'\') ? \'\' : this.value;" value="Search '.$action[0].'" name="s" value="' . esc_attr($term) . '" class="text dw_search_input" />
+			<label class="screen-reader-text" >' . _e('Search '.$action[0]) . '</label>'.$go_button),
+			'href' => '#' ) );
+
+			$wp_admin_bar->add_menu( array( 
+			'parent' => $action[2], 
+			'title' => 'Add New '.$action[3], 
+			'href' => admin_url($link) ) );
+		}
+	}
 
 	// Only add remaining menu items if we're not in wp-admin.
 	if ( is_admin() )
 	return;		
 
 	$wp_admin_bar->add_menu( array(
-	'id' => 'posts_stuff',
-	'parent' => 'dsgnwrks_help_menu',
-	'title' => __( 'Posts'),
-	'href' => $admin_url.'edit.php' ) );
-
-	$wp_admin_bar->add_menu( array(
-	'id' => 'page_stuff',
-	'parent' => 'dsgnwrks_help_menu',
-	'title' => __( 'Pages'),
-	'href' => $admin_url.'edit.php?post_type=page' ) );
-
-	$wp_admin_bar->add_menu( array(
-	'id' => 'plugins_stuff',
-	'parent' => 'dsgnwrks_help_menu',
-	'title' => __( 'Plugins'),
-	'href' => $admin_url.'plugins.php' ) );
-
-	$wp_admin_bar->add_menu( array(
-	'parent' => 'plugins_stuff',
-	'title' => __( 'Add New Plugin' ),
-	'href' => $admin_url.'plugin-install.php' ) );
-
-	$wp_admin_bar->add_menu( array(
-	'parent' => 'dsgnwrks_help_menu',
-	'title' => __( 'Media'),
-	'href' => $admin_url.'upload.php' ) );
-
-	$wp_admin_bar->add_menu( array(
+	'id' => 'settings_stuff',
 	'parent' => 'dsgnwrks_help_menu',
 	'title' => __( 'Settings'),
-	'href' => $admin_url.'options-general.php' ) );
+	'href' => admin_url('options-general.php') ) );
+
+	$wp_admin_bar->add_menu( array(
+	'parent' => 'settings_stuff',
+	'title' => __( 'Writing'),
+	'href' => admin_url('options-writing.php') ) );
+
+	$wp_admin_bar->add_menu( array(
+	'parent' => 'settings_stuff',
+	'title' => __( 'Reading'),
+	'href' => admin_url('options-reading.php') ) );
+
+	$wp_admin_bar->add_menu( array(
+	'parent' => 'settings_stuff',
+	'title' => __( 'Discussion'),
+	'href' => admin_url('options-discussion.php') ) );
+
+	$wp_admin_bar->add_menu( array(
+	'parent' => 'settings_stuff',
+	'title' => __( 'Media'),
+	'href' => admin_url('options-media.php') ) );
+
+	$wp_admin_bar->add_menu( array(
+	'parent' => 'settings_stuff',
+	'title' => __( 'Privacy'),
+	'href' => admin_url('options-privacy.php') ) );
+
+	$wp_admin_bar->add_menu( array(
+	'parent' => 'settings_stuff',
+	'title' => __( 'Permalinks'),
+	'href' => admin_url('options-permalink.php') ) );
 }
 
 // add theme info dashboard widget
--- readme.txt
+++ readme.txt
@@ -1,23 +1,25 @@
 === Plugin Name ===
-Enhanced Admin Bar with Codex Search
+Enhanced Admin Bar with Codex Search and Custom Menus
 
 Contributors: jtsternberg
-Plugin Name: Enhanced Admin Bar with Codex Search
+Plugin Name: Enhanced Admin Bar with Codex Search and Custom Menus
 Plugin URI: http://dsgnwrks.pro/enhanced-admin-bar-with-codex-search/
-Tags: Admin Bar, Codex Search, Search, Admin, adminbar, bar, topbar, plugin search, dashboard widget
+Tags: Admin Bar, Codex Search, Search, Admin, adminbar, bar, topbar, plugin search, dashboard widget, wpbeginner, custom menu, menus
 Author URI: http://dsgnwrks.pro
 Author: DsgnWrks
 Donate link: http://dsgnwrks.pro/give/
 Requires at least: 3.0
 Tested up to: 3.2
-Stable tag: 1.0
-Version: 1.0
+Stable tag: 1.5
+Version: 1.5
 
-This plugin provided easy access to the codex, plugins repository and common wp-admin areas via the 3.1 Admin Bar.
+This plugin adds convenient search fields to provide easy access to the codex, wpbeginner, and common wp-admin areas via the 3.1 Admin Bar. Also, add your own custom menu to the Admin Bar.
 
 == Description ==
 
-This simple plugin enhances the default WordPress admin bar by adding custom menu that includes a search field for searching the Codex, and search field for searching the plugin repository. Links to common areas in wp-admin (posts, pages, plugins, media, settings) are included in the drop down menu when you are not in the WordPress admin area.
+This simple plugin enhances the default WordPress admin bar by adding a new menu that includes search fields for searching the Codex, wpbeginner.com, plugins repository, themes repository etc. Links and search fields to common areas in wp-admin (posts, pages, custom post types, plugins, media, settings) are included in the drop down menu when you are not in the WordPress admin area. The plugin search box now takes you to the WordPress internal plugins search page rather than wordpress.org.
+
+If that's not enough link goodness for you, the plugin now has the option for a custom WordPress menu in the WordPress Admin Bar. (Props to <a href="https://twitter.com/wpsnipp" target="_blank">@wpsnipp</a> - <a href="http://wpsnipp.com/" target="_blank">http://wpsnipp.com/</a>) If you go to wp-admin/nav-menus.php you'll see a theme location, "Admin Bar Custom Navigation Menu" where you can attach a custom menu.
 
 The plugin also includes a dashboard widget that conveniently displays info about the currently active theme.
 
@@ -27,21 +29,34 @@ The plugin also includes a dashboard widget that conveniently displays info abou
 1. Upload the `admin-bar-enhancements` directory to the `/wp-content/plugins/` directory.
 1. Activate the plugin through the 'Plugins' menu in WordPress.
 2. Admin bar should be updated with new menu.
+3. If you would like a custom menu in the admin bar, navigate to "Menus" under "Appearance." You'll see a theme location, "Admin Bar Custom Navigation Menu" where you can attach a custom menu.
 
 == Frequently Asked Questions ==
 
-This plugin provided easy access to the codex, plugins repository and common wp-admin areas via the 3.1 Admin Bar.
+This plugin adds convenient search fields to provide easy access to the codex, wpbeginner, and common wp-admin areas via the 3.1 Admin Bar. Also, add your own custom menu to the Admin Bar.
 
 == Screenshots ==
 
-1. Menu view from your site's front-end.
-2. Menu view from WordPress Admin back-end.
-2. Menu view from WordPress Version 3.2+ Admin back-end. (as of this plugin's creation, the admin bar is not enabled in the back-end in version 3.2+)
+1. Menu view from your site's front-end. Posts and Pges are displayed, but if you have custom post types, they would also show up in this list with a search field / add new link on hover.
+2. Menu view from WordPress Admin back-end. Again, if you have custom post types, the corresponding search field would show up in this list.
+3. Menu view from WordPress Version 3.2+ Admin back-end. (as of this plugin's creation, the admin bar is not enabled in the back-end in version 3.2+) the Plugin search box now takes you to the WordPress internal plugins search page rather than wordpress.org.
+4. View of WordPress built-in menu system with a custom menu added to the Admin Bar
+5. View of Admin Bar with custom menu added.
 
 == Changelog ==
 
-This plugin provided easy access to the codex, plugins repository and common wp-admin areas via the 3.1 Admin Bar.
+= 1.5 =
+* Plugins search now takes you to the WordPress internal plugins search page rather than wordpress.org.
+* Added search for: wpbeginner.com Themes, Media, Pages, Posts, Custom Post Types.
+* Added an "Upload" menu item to Plugins, Themes, and Media.
+* Added an "Add New" menu item to Pages, Posts, Custom Post Types.
+* Added Settings sub-menus to the Settings dropdown in the Admin Bar Menu.
+* Added an option to add a custom menu to the Admin Bar from the built-in WordPress menu system.
 
-== Upgrade Notice == 
 
-This plugin provided easy access to the codex, plugins repository and common wp-admin areas via the 3.1 Admin Bar.
+== Upgrade Notice ==
+
+= 1.5 =
+Upgrade for more search options, and option to add your own custom menu.
+
+== Arbitrary section ==
